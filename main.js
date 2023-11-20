463c31cf11854e2dc236e3d94b549162330e5e09
SefvandenHeurik <Sef.van.den.Heurik@student.gildeopleidingen.nl>
2023-11-20 11:34:48 +0100

    test

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.getElementById('demo').innerHTML = 'Hello JavaScript';
+function openTab(tabName) {
+    // Verberg alle tabbladen
+    var tabs = document.getElementsByClassName("tabContent");
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = "none";
+    }
+  
+    // Toon het gekozen tabblad
+    document.getElementById(tabName).style.display = "block";
+  }
