1e3ee44705667ed1ea63aaf56de0788f8cd47f9f
SefvandenHeurik <Sef.van.den.Heurik@student.gildeopleidingen.nl>
2023-11-22 14:59:04 +0100

    Nieuw

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ function openTab(tabName) {
     // Toon het gekozen tabblad
     document.getElementById(tabName).style.display = "block";
   }
+
+function redirectToIndex() {
+    // Redirect to index.html
+    window.location.href = "index.html";
+}
+function redirectToforgetpassword() {
+    // Redirect to forgetpassword.html
+    window.location.href = "forgetpassword.html";
+}
+
+function redirectToInlogmain() {
+    // Redirect to Inlogmain.html
+    window.location.href = "Inlogmain.html";
+}
+
+function redirectToInlogmain() {
+  // Redirect to Inlogmain.html
+  window.location.href = "Inlogmain.html";
+}
