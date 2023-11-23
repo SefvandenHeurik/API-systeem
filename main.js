5b1555a4a95399b0dc6a5da1f7ed69586d33b191
Freekiezz <freek.driessen05@gmail.com>
2023-11-23 14:31:35 +0100

    commit

--- main.js
+++ main.js
@@ -31,28 +31,4 @@ function redirectToInlogmain() {
 function redirectToInlogmain() {
   // Redirect to inlogmain.html
   window.location.href = "inlogmain.html";
-
-function openTab(tabName) {
-    // Verberg alle tabbladen
-    var tabs = document.getElementsByClassName("tabContent");
-    for (var i = 0; i < tabs.length; i++) {
-      tabs[i].style.display = "none";
-    }
-  
-    // Toon het gekozen tabblad
-    document.getElementById(tabName).style.display = "block";
-  }
-
-function redirectToIndex() {
-    // Redirect to index.html
-    window.location.href = "index.html";
-}
-function redirectToforgetpassword() {
-    // Redirect to forgetpassword.html
-    window.location.href = "forgetpassword.html";
-}
-
-function redirectToInlogmain() {
-    // Redirect to Inlogmain.html
-    window.location.href = "Inlogmain.html";
 }
