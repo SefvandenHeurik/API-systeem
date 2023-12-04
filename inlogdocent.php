43a64c85e8d9783dfc4180e92e1aff21f93132c6
Freekiezz <freek.driessen05@gmail.com>
2023-12-04 13:01:03 +0100

    yes

--- inlogdocent.php
+++ inlogdocent.php
@@ -1,99 +1,88 @@
 <?php
-
 @include 'config.php';
 
 session_start();
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
-   $email = isset($_POST['email']) ? mysqli_real_escape_string($conn, $_POST['email']) : '';
-   $pass = isset($_POST['password']) ? md5($_POST['password']) : '';
-   
-   // Note: Avoid using md5 for password hashing, use more secure methods like password_hash
+    $email = isset($_POST['email']) ? mysqli_real_escape_string($conn, $_POST['email']) : '';
+    $pass = isset($_POST['password']) ? md5($_POST['password']) : '';
 
-   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+    // Note: Avoid using md5 for password hashing, use more secure methods like password_hash
 
-   $result = mysqli_query($conn, $select);
+    $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
 
-   if(mysqli_num_rows($result) > 0){
+    $result = mysqli_query($conn, $select);
 
-      $row = mysqli_fetch_array($result);
+    if (mysqli_num_rows($result) > 0) {
 
-      if($row['user_type'] == 'admin'){
+        $row = mysqli_fetch_array($result);
 
-         $_SESSION['admin_name'] = $row['name'];
-         header('location:docenten.php');
+        if ($row['user_type'] == 'admin') {
 
-      }elseif($row['user_type'] == 'user'){
+            $_SESSION['admin_name'] = $row['name'];
+            header('location: docenten.php');
+        } elseif ($row['user_type'] == 'user') {
 
-         $_SESSION['user_name'] = $row['name'];
-         header('location:docenten.php');
-
-      }
-     
-   }else{
-      $error[] = 'Incorrect email or password!';
-   }
-
-};
+            $_SESSION['user_name'] = $row['name'];
+            header('location: docenten.php');
+        }
+    } else {
+        $error[] = 'Incorrect email or password!';
+    }
+}
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="main.js"></script>
+    <title>Docenten Login</title>
 </head>
 
 <body>
-    
+
     <div class="containerdocenteninlog">
-    
+
         <div>
             <p>Docenten Login</p>
         </div>
 
-
         <div class="icon">
-            
+            <svg>
                 <path d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
                     fill="currentColor"></path>
             </svg>
         </div>
-        </button>
-    
+
         <?php
-             if(isset($error)){
-                foreach($error as $errorMessage){
-                    echo '<span class="error-msg">'.$errorMessage.'</span>';
-                 };
+        if (isset($error)) {
+            foreach ($error as $errorMessage) {
+                echo '<span class="error-msg">' . $errorMessage . '</span>';
             };
+        }
         ?>
 
-        <div class="loginInputFieldDocenten">
-            <input type="email" placeholder="Email.." id="email">
-        </div>
+        <form method="POST" action="">
+            <div class="loginInputFieldDocenten">
+                <input type="email" name="email" placeholder="Email.." id="email">
+            </div>
 
-        <div class="loginInputFieldDocenten">
-            <input type="password" placeholder="Password.." id="password">
-        </div>
+            <div class="loginInputFieldDocenten">
+                <input type="password" name="password" placeholder="Password.." id="password">
+            </div>
 
-        <div id="loginForgotPasswordDocenten" style="display: flex;">
-            <a href="#" class="loginForgotPasswordDocenten">Register</a>
-            <button onclick="redirectToregister()"></button>
-        </div>
-
-        <div id="loginDocentenButton">
-            <button onclick="redirectToDash()">Login</button>
-        </div>
+            <div id="loginForgotPasswordDocenten" style="display: flex;">
+                <a href="#" class="loginForgotPasswordDocenten">Register</a>
+                <button type="submit" name="submit">Login</button>
+            </div>
+        </form>
     </div>
-
-
 
 </body>
 
