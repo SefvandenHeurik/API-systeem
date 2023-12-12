3fd120e58821e1966318b0f3cb42996524534889
Vincentosz <Vincent.Steegh@student.gildeopleidingen.nl>
2023-12-12 09:03:09 +0100

    Update inlogdocent.php

--- inlogdocent.php
+++ inlogdocent.php
@@ -57,13 +57,7 @@ if (isset($_POST['submit'])) {
             <p>Docenten Login</p>
         </div>
 
-        <div class="icon">
-            <svg>
-                <path d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                    fill="currentColor"></path>
-            </svg>
-        </div>
+    
 
         <?php
         if (isset($error)) {
